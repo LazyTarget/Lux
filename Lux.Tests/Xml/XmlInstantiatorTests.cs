40f8da339ffc7b4d567c6c1dc2be48c2ef69fb20
LazyTarget <peter.aslund@hotmail.com>
2016-01-22 19:57:56 +0100

    Tests project

---
+++ Lux.Tests/Xml/XmlInstantiatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Lux.Xml;
+using NUnit.Framework;
+
+namespace Lux.Tests.Xml
+{
+    [TestFixture]
+    public class XmlInstantiatorTests
+    {
+        private XmlInstantiator GetSUT()
+        {
+            return new XmlInstantiator();
+        }
+
+
+        [TestCase]
+        public void InstantiateElement_CanInstantiateValue_WhenString()
+        {
+            var sut = GetSUT();
+
+            var expected = "Some text";
+            var elem = new XElement("tag");
+            elem.SetValue(expected);
+
+            var actual = sut.InstantiateElement(elem);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase]
+        public void InstantiateElement_CanInstantiateValue_WhenInteger()
+        {
+            var sut = GetSUT();
+
+            var expected = 13;
+            var elem = new XElement("tag");
+            elem.SetValue(expected);
+
+            var actual = sut.InstantiateElement(elem);
+            Assert.AreEqual(expected.ToString(), actual);
+        }
+
+    }
+}
