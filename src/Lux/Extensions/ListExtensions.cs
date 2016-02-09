4b30274df18d490cad8be2e2de8a9478d8f92cae
LazyTarget <peter.aslund@hotmail.com>
2016-02-09 23:03:43 +0100

    Using .NET 4.5 (instead of .NET 4.6)

--- src/Lux/Extensions/ListExtensions.cs
+++ src/Lux/Extensions/ListExtensions.cs
@@ -11,5 +11,23 @@ namespace Lux.Extensions
             l.Sort(comparer);
             return l;
         }
+
+        
+        public static T[] Append<T>(this T[] array, T obj)
+        {
+            var objects = new T[] {obj};
+            var res = Append<T>(array, objects);
+            return res;
+        }
+
+        public static T[] Append<T>(this T[] array, params T[] objects)
+        {
+            var arr = (array ?? new T[0]);
+            if (objects == null)
+                return arr;
+            var res = arr.Concat(objects).ToArray();
+            return res;
+        }
+
     }
 }
