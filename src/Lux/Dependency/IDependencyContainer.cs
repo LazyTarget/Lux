2ef03a3620d7286f17850f9aa6d95fdeeab6c4b9
LazyTarget <peter.aslund@hotmail.com>
2016-02-27 03:29:34 +0100

    Dependency Injection lib

---
+++ src/Lux/Dependency/IDependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lux.Dependency
+{
+    public interface IDependencyContainer : IDisposable
+    {
+        IDependencyContainer Parent { get; }
+        IDependencyContainer CreateChildContainer();
+
+        IDependencyContainer RegisterInstance(Type type, string name, object instance, object settings);
+        IDependencyContainer RegisterType(Type from, Type to, string name, object settings);
+        object GetRegistrations();
+        object Resolve(Type type, string name, object settings);
+    }
+}
--- src/Lux/FrameworkConfig.cs
+++ src/Lux/FrameworkConfig.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Xml.Linq;
 using Lux.Config.Xml;
+using Lux.Dependency;
 using Lux.Xml;
 
 namespace Lux
@@ -16,28 +18,75 @@ namespace Lux
         }
 
 
+        private AssignableVariable<IDependencyContainer> _dependencyContainer =
+            new AssignableVariable<IDependencyContainer>();
+
+        public IDependencyContainer DependencyContainer
+        {
+            get { return _dependencyContainer?.Value; }
+            set { _dependencyContainer.Value = value; }
+        }
+
+        public FrameworkConfig()
+        {
+            
+        }
+
+
         public virtual void Apply()
         {
             if (_cultureInfo.Assigned)
                 Framework.CultureInfo = CultureInfo;
+            if (_dependencyContainer.Assigned)
+                Framework.DependencyContainer = DependencyContainer;
         }
 
         public override void Configure(XElement element)
         {
+            XElement elem;
             if (element == null)
                 throw new ArgumentNullException(nameof(element));
 
-            var elem = element.Element("cultureInfo");
+            elem = FindElement(element, nameof(CultureInfo));
             if (elem != null)
             {
                 var name = elem.GetAttributeValue("value") ?? elem.Value;
                 CultureInfo = CultureInfo.GetCultureInfo(name);
             }
+
+            elem = FindElement(element, nameof(DependencyContainer));
+            if (elem != null)
+            {
+                var typeStr = elem.GetAttributeValue("type");
+                var type = Type.GetType(typeStr, true);
+                if (!typeof (IDependencyContainer).IsAssignableFrom(type))
+                    throw new InvalidCastException("Invalid type");
+
+                object obj;
+                if (Framework.TypeInstantiator != null)
+                    obj = Framework.TypeInstantiator.Instantiate(type);
+                else
+                    obj = Activator.CreateInstance(type);
+
+                var instance = (IDependencyContainer) obj;
+                DependencyContainer = instance;
+            }
         }
 
         public override void Export(XElement element)
         {
-                
+            
+        }
+
+
+        private XElement FindElement(XElement element, XName name)
+        {
+            var elem = element.Elements()
+                .FirstOrDefault(
+                    x =>
+                        x.Name.LocalName.Equals(name.LocalName,
+                            StringComparison.InvariantCultureIgnoreCase));
+            return elem;
         }
     }
 }
