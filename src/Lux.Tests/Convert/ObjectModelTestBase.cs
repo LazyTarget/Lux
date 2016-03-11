fab432420d74927cca23cd7c565cd80249bf822b
LazyTarget <peter.aslund@hotmail.com>
2016-03-11 09:01:16 +0100

    Converter tests

---
+++ src/Lux.Tests/Convert/ObjectModelTestBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using Lux.Interfaces;
+using Lux.Tests.Model.Models;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Lux.Tests.Convert
+{
+    [TestFixture]
+    public class ConverterTests : TestBase
+    {
+        protected virtual IConverter GetSUT()
+        {
+            return new Converter();
+        }
+
+
+        [TestCase]
+        public virtual void CanConvertStringToEnum()
+        {
+            var expected = StringComparison.InvariantCultureIgnoreCase;
+            var targetType = expected.GetType();
+            var input = expected.ToString();
+
+            var sut = GetSUT();
+            var actual = sut.Convert(input, targetType);
+
+            Assert.IsInstanceOf(targetType, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase]
+        public virtual void CanConvertIntToEnum()
+        {
+            var expected = StringComparison.InvariantCultureIgnoreCase;
+            var targetType = expected.GetType();
+            var input = (int)expected;
+
+            var sut = GetSUT();
+            var actual = sut.Convert(input, targetType);
+
+            Assert.IsInstanceOf(targetType, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase]
+        public virtual void CanConvertStringToFlagsEnum()
+        {
+            var expected = BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetProperty;
+            var targetType = expected.GetType();
+            var input = expected.ToString();
+
+            var sut = GetSUT();
+            var actual = sut.Convert(input, targetType);
+
+            Assert.IsInstanceOf(targetType, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase]
+        public virtual void CanConvertIntToFlagsEnum()
+        {
+            var expected = BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetProperty;
+            var targetType = expected.GetType();
+            var input = (int)expected;
+
+            var sut = GetSUT();
+            var actual = sut.Convert(input, targetType);
+
+            Assert.IsInstanceOf(targetType, actual);
+            Assert.AreEqual(expected, actual);
+        }
+        
+    }
+}
