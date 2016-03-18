762b26d7253bedfdf944547750421cca44c1bf45
LazyTarget <peter.aslund@hotmail.com>
2016-03-18 16:46:25 +0100

    Refactored NuGet specs and scripts

--- NuGet/nupack.bat
+++ NuGet/nupack.bat
@@ -1,5 +1,11 @@
-call .\build.bat
-copy .\Lux.nuspec .\package\Lux.nuspec
-nuget pack .\package\Lux.nuspec
-mkdir .\releases
-move .\Lux.*.nupkg .\releases\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ..\src\Lux.sln /p:Configuration=Release /m
+
+cd /d %~dp0
+cd Lux
+call .\nupack.bat
+
+cd /d %~dp0
+cd Lux.Diagnostics.Log4net
+call .\nupack.bat
+
+cd /d %~dp0
